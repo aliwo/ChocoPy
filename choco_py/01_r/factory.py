e9be60c5996b7f6e4119addd970af81e626579b7
aliwo <aliwo@naver.com>
2019-08-17 19:49:21 +0900

    웹툰 불러오기 수정

---
+++ choco_py/01_r/factory.py
@@ -0,0 +1,23 @@
+# 이곳은 초코PY 공장입니다!
+
+# 공장의 총 재료량
+kakao = 1000
+sugar = 1000
+milk = 1000
+
+
+choco = 60 + 120 + 100
+milk_choco = 60 + 120 + 200
+dark_choco = 90 + 120 + 100
+
+
+# choco
+def make_choco():
+    choco -= 60
+    sugar -= 120
+    milk -= 100
+
+make_choco()
+
+
+
