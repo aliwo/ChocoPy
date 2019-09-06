7b5e4dfed40e18150210c9e3106d4f34682595a9
aliwo <aliwo@naver.com>
2019-09-06 15:38:26 +0900

    shop sell 함수 작성중...

--- choco_py/complete/shop.py
+++ choco_py/complete/shop.py
@@ -1,6 +1,12 @@
 
 class Shop():
 
+    choco_price = {
+        '초코': 1000,
+        '밀크초코': 1500,
+        '다크초코': 2000
+    }
+
     def __init__(self, warehouse):
         self.warehouse = warehouse
 
@@ -8,7 +14,25 @@ class Shop():
         '''
         초코 객체와 거스름돈이 담긴 dict 를 리턴
         '''
-        result = []
+
+        # 돈이 충분한지 먼저 계산합니다.
+        # 모든 초코의 재고가 충분한지 계산하고
+        # 충분하다면 choco 를 pop 해서 담은 후 return 합니다.
+
+        result = {
+            'chocos': [],
+            'change': money
+        }
+
         for key, value in order_spec:
-            if key == '초코':
-                self.warehouse.pop()
+            if self.warehouse.peek(key) < value:
+                return # 재고 부족!
+            result['change'] -= (self.choco_price[key] * value)
+            if result['change'] < 0:
+                return # 금액 부족!
+
+        for key, value in order_spec:
+            for _ in range(value):
+                result['chocos'].append(self.warehouse.pop(key))
+
+        return result
